a14a14701671e3aac40ef349e8a0562a9ea11ba2
cagdasscetin <cagdasscetin@gmail.com>
2024-12-16 23:05:26 +0300

    The "Sprout Class" Technique

---
+++ SproutClass.Tests/EmployeesTest.cs
@@ -0,0 +1,17 @@
+using Moq;
+
+namespace SproutClass.Tests;
+
+public class EmployeesTest
+{
+    [Fact]
+    public void GetEmployeesData_ReturnsSalaries()
+    {
+        var currentDataObject = new Mock<IEmployeeService>();
+        currentDataObject.Setup(x => x.GetEmployeesData()).Returns([new Employee { UniqueId = 1, Salary = 100 }]);
+        
+        var salaryData = currentDataObject.Object.GetEmployeesData();
+        
+        Assert.Contains(salaryData, x => x.Salary > 0);
+    }
+}
